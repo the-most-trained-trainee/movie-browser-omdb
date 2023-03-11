7001cf86afdddc4230609ec04b968e1f3f801177
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2023-03-12 00:25:48 +0200

    interim commit

---
+++ app/page.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import styles from "./page.module.css";
+
+// import type { RootState } from './GlobalRedux/store';
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "./GlobalRedux/Features/counter/counterSlice";
+
+export default function Home() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <main className={styles.main}>
+      <button className={styles.button} onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <span>{count}</span>
+      <button className={styles.button} onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => dispatch(incrementByAmount(2))}>
+        Increment by 2
+      </button>
+    </main>
+  );
+}
